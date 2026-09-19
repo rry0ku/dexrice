6332e3e369d2aac133fd8ab7e0a1b3641cd383b1
rry0ku <ishmeetsingharora07@gmail.com>
2026-09-19 19:33:20 +0530

    files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- General
-config.font_size = 15.0
+config.font_size = 14.0
 config.line_height = 1.2
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 
